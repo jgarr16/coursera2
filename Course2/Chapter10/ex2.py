ae67b263003bccb3e448b01c9003c1289272178e
jrgarrigues <jgarr16-github@yahoo.com>
2017-06-10 11:28:18 -0400

    completed ex2.py

--- Course2/Chapter10/ex2.py
+++ Course2/Chapter10/ex2.py
@@ -14,7 +14,18 @@ for line in fhand:
     words = line.split()
     if (len(words) >= 5) and (words[0] == 'From'): 
 		domain = words[5].split(':')
-		print domain[0]
+		# print domain[0]      # just for testing, feel free to delete
 		if domain[0] not in daycount: daycount[domain[0]] = 1
 		else: daycount[domain[0]] += 1
-print daycount
+
+# sort the dict() by value
+lst = list()
+for time, count in daycount.items():
+	lst.append((time, count))
+
+	lst.sort(reverse=False)
+	
+for time, count in lst:
+	print time, count
+
+

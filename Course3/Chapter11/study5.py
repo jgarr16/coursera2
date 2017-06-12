af6c4d819a50e4047bf9d3f107e17c4d5b242b9e
jrgarrigues <jgarr16-github@yahoo.com>
2017-06-11 20:18:56 -0400

    starting exercises for ch11

--- Course3/Chapter11/study5.py
+++ Course3/Chapter11/study5.py
@@ -1,11 +1,9 @@
 # this program categorizes the mbox-short.txt email string by the day that the message was
 # sent. it will count the number of messages by the day that they were sent.
 
-fhand = open('mbox-short.txt')
-daycount = dict()
-for line in fhand:
-    words = line.split()
-    if (len(words) >= 3) and (words[0] == 'From'): 
-		if words[2] not in daycount: daycount[words[2]] = 1
-		else: daycount[words[2]] += 1
-print daycount
+import re
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('^From .* ([0-9][0-9]):', line)
+    if len(x) > 0 : print x

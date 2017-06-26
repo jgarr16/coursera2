97ef2bed40f48a16285e9039abce97a46c53088e
jrgarrigues <jgarr16-github@yahoo.com>
2017-06-26 19:40:38 -0400

    upgrading to py3 w unit tests

---
+++ pycourse1/Chapter2/ex2py3.py
@@ -0,0 +1,20 @@
+# this program prompts the user for a name and then greets them, on screen, by name. 
+
+def namePrompt():
+	name = input('What is your name? ')
+	
+	if len(name) < 1:
+		print()
+		print('Cat got your tongue? Try again!')
+		print()
+	else:
+		try:
+			print()
+			print('Hello ' + name)
+			print()
+		except:
+			print('Sorry, I ran into a problem there. Please try again...')
+			exit()
+
+namePrompt()
+
